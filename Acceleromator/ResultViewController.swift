85824b8d06fb8d93ed922ca3aad6d8db262a0fe6
ayu129 <ayu.daddybear.29@gmail.com>
2021-02-07 13:24:01 +0900

    加速度センサー

---
+++ Acceleromator/ResultViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ResultViewController.swift
+//  Acceleromator
+//
+//  Created by 伊藤愛結 on 2021/02/07.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var accelerationX: Double!
+    @IBOutlet var label: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let result : Double = fabs(accelerationX*100)
+        label.text = String(format: "%.1f°", result)
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
